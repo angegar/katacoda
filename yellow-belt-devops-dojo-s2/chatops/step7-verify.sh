0d0b8753e971d90b9fe207e4925f3f497cdd8671
angegar <lg7983@yahoo.com>
2019-03-05 12:00:32 +0100

    Add step7 checks

--- yellow-belt-devops-dojo-s2/chatops/step7-verify.sh
+++ yellow-belt-devops-dojo-s2/chatops/step7-verify.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-[ -d ~/demo-script ]&& cd ~/demo-script && [ -d ~/myhubot/node-module/hubot-demo-script ] && echo 'done'
+[ -d ~/demo-script ] && cd ~/demo-script && \
+[ -f ~/demo-script/src/demo-script.js ] && \
+grep -q hubot-demo-script ~/myhubot/external-scripts.json  && \
+[ -d ~/myhubot/node_modules/hubot-demo-script ] && echo 'done'
